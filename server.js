132ac30b16511492e5c2dcb464240b1c74ca64e0
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-05-06 13:22:14 +0200

    Add client to server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const messange = [];
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+  });
+
+app.use((req, res) => {
+  res.status(404);
+  res.json({messange: 'Not found...'});
+});
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+});
