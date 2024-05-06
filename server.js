9ef5ed05ec76739d4034e783b887c5deb9c6ba87
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-05-06 13:36:39 +0200

    Add client

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const messange = [];
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
 
 app.use((req, res) => {
